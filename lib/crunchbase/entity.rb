5dad674dd8b42b6f5a3797e1bfc88613e8aa0ae9
mattwalters <matthewrobertwalters@gmail.com>
2014-05-09 16:08:52 -0700

    maybe thatll work

--- lib/crunchbase/entity.rb
+++ lib/crunchbase/entity.rb
@@ -17,7 +17,7 @@ module Crunchbase
 	raise NotImplementedError
       end
       def find(permalink)
-	new("path" => "#{entity_type}/#{permalink}")
+	new("path" => "#{self.entity_type}/#{permalink}")
       end
     end
 
